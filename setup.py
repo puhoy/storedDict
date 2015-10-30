62ea66438f4f9464e6456871c307621df21e2865
jan <jan@kwoh.de>
2015-10-30 12:45:16 +0100

    tagging

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
   name = 'storedDict',
-  packages = ['storedDict'], # this must be the same as the name above
-  version = '0.1',
+  packages = ['storedDict'],
+  version = '0.2',
   description = 'store dict as json',
   author = 'Jan Hartmann',
   author_email = 'stuff@kwoh.de',
   url = 'https://github.com/puhoy/storedDict',
-  download_url = 'https://github.com/puhoy/storedDict/tarball/0.2',
-  keywords = ['storage'], # arbitrary keywords
+  download_url = 'https://github.com/puhoy/storedDict/tarball/0.2.1',
+  keywords = ['storage'],
   classifiers = [],
 )
