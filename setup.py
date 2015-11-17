7785059c2c11532e9092103b3d6b88c9873bdd9f
jan <jan@kwoh.de>
2015-11-17 21:25:51 +0100

    0.2.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'storedDict',
   packages = ['storedDict'],
-  version = '0.2.3',
+  version = '0.2.4',
   description = 'store dict as json',
   author = 'Jan Hartmann',
   author_email = 'stuff@kwoh.de',
