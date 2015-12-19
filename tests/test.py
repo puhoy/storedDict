a1d3916056da3f39a25f9b1b53d38e1d11a0ae53
jan <jan@kwoh.de>
2015-12-19 09:54:33 +0100

    new test

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,19 @@ from storedDict import StoredDict
 
 class TestStoredDict(unittest.TestCase):
 
+    def test_save_and_load(self):
+        val = 1
+        sd = StoredDict('testing.json')
+        sd['one'] = val
+        sd.commit()
+        sd = StoredDict('testing.json')
+        self.assertEqual(val, sd['one'])
+        val2 = 2
+        sd['two'] = 2
+        sd.commit()
+        self.assertEqual(val, sd['one'])
+        self.assertEqual(val2, sd['two'])
+
     def test_autovivification(self):
         sd = StoredDict('testing.json')
         sd['one']['two']['three'] = 3
@@ -34,12 +47,14 @@ class TestStoredDict(unittest.TestCase):
         self.assertEqual(val, loaded_sd['one']['two']['three'])
 
     def test_load_saved_dict(self):
-        sd2 = StoredDict('testing2.json')
+        sd2 = StoredDict('tests/testing2.json')
+        sd2.commit()
         self.assertEqual(3, sd2['one']['two']['three'])
 
     def tearDown(self):
         try:
             os.remove('testing.json')
+            pass
         except:
             pass
 
